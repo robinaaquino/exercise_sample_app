b0d942c112a41983ff48a090965e628d3ffff80e
Robina Rhamz Aquino <robinarhamzaquino@gmail.com>
2023-02-13 23:59:19 +0800

    refactor: used erb to reduce redundancy

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -21,9 +21,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get about" do
     get static_pages_about_url
     assert_select "title", "About | #{@base_title}"
-
     assert_response :success
   end
 
-
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_select "title", "Contact | #{@base_title}"
+    assert_response :success
+  end
 end
