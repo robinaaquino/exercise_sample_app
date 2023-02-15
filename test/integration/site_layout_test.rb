61e9e69761da004d3dd86a17f756ad002e6df6ab
Robina Rhamz Aquino <robinarhamzaquino@gmail.com>
2023-02-15 17:42:39 +0800

    feat: implemented signup_path and related tests

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -12,8 +12,15 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", signup_path
 
     get contact_path
     assert_select "title", full_title("Contact")
+  end
+
+  test "signup link" do
+    get root_path
+    get signup_path
+    assert_select "title", full_title()
   end
 end
