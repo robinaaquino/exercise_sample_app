48fa60e4f8f2a75a157638fe9d4e2f7a07e913b3
Robina Rhamz Aquino <robinarhamzaquino@gmail.com>
2023-02-16 17:25:54 +0800

    feat: added styles for showing individual user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
   def new
+    # debugger
   end
 
   def show # method used to navigate to show.html.erb and get the id from params to find the user
     @user = User.find(params[:id])
-    debugger
+    # debugger
   end
 
 
