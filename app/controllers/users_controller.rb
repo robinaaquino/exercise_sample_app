c49913f0beb8dffd762c23ff88aadc7144dde27d
Robina Rhamz Aquino <robinarhamzaquino@gmail.com>
2023-02-16 17:13:06 +0800

    feat: implemented show page for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def show # method used to navigate to show.html.erb and get the id from params to find the user
+    @user = User.find(params[:id])
+    debugger
+  end
+
+
+
 end
