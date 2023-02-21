9adbde1383a7aea0854172eacef64b99db99433b
Robina Rhamz Aquino <robinarhamzaquino@gmail.com>
2023-02-21 16:27:27 +0800

    refactor: redefined digest and new_token as class methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,20 +12,21 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 6 }
   has_secure_password
 
-  # Returns the hash digest of the given string.
-  def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
-  end
-
-  # Returns a random token
-  def User.new_token
-    SecureRandom.urlsafe_base64
-  end
-
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
+  class << self
+    # Returns the hash digest of the given string.
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+
+    # Returns a random token
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
 end
