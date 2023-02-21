d9c76b9865922fd3ee54fcfbd03d27f202eae5e0
Robina Rhamz Aquino <robinarhamzaquino@gmail.com>
2023-02-21 11:46:56 +0800

    feat: locked features behind user login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  before_action :logged_in_user, only: [:create, :destroy, :new]
+
   def new
   end
 
@@ -20,4 +22,12 @@ class SessionsController < ApplicationController
     log_out
     redirect_to root_url
   end
+
+  private
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
 end
