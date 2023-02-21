d359e29cf4c902f5eeb1dee514ddadb994763a4d
Robina Rhamz Aquino <robinarhamzaquino@gmail.com>
2023-02-21 16:24:15 +0800

    feat: added remember_digest to Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_15_160658) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_21_071257) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
