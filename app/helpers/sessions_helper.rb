4004813daf197b290303f7939ee159c821777a53
Robina Rhamz Aquino <robinarhamzaquino@gmail.com>
2023-02-22 10:51:57 +0800

    feat: remember method for sessions helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,6 +4,13 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  #Remembers a user for sessions
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
   def current_user
     if session[:user_id]
       @current_user ||= User.find_by(id: session[:user_id])
