60744df0ce9f0309a66cc71c0fab7dcd3877b160
Robina Rhamz Aquino <robinarhamzaquino@gmail.com>
2023-02-22 11:49:31 +0800

    test: added tests and fixed errors with session helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -22,9 +22,9 @@ module SessionsHelper
     if session[:user_id]
       @current_user ||= User.find_by(id: session[:user_id])
     elsif (user_id = cookies.signed[:user_id])
-      user = User.find_by( :user_id)
+      user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
-        login_in user
+        log_in user
         @current_user = user
       end
     end
