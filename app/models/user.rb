8f489141b312ce6f38e65028e5172d63a9a9a997
Robina Rhamz Aquino <robinarhamzaquino@gmail.com>
2023-02-22 10:47:58 +0800

    feat: added authenticated method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,11 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
+  #Returns true if the given token matches the digest
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
   class << self
     # Returns the hash digest of the given string.
     def digest(string)
